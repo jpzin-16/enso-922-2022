78a295a05aa1762e2b5850492097693ee4769f5e
JPDdeOliveira <jpos2@aluno.ifal.edu.br>
2022-09-26 19:50:06 +0000

    Adaptando conforme revisão

--- API-CALCULADORA/api/src/test/java/app/SomaTest.java
+++ API-CALCULADORA/api/src/test/java/app/SomaTest.java
@@ -1,11 +1,9 @@
 package app;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
 public class SomaTest{
     
     Adicao adicao;
